e71dfcff458fd55ef8da2af86f1669d0fdeb3842
Adharsh R <adharshrajeeev2000@gmail.com>
2023-04-12 21:04:01 +0530

    req added

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,9 @@ const { SignUpUser,LoginUser } = require('../controllers/userController');
 
 
 router.post('/signUp',SignUpUser);
-
+router.get('/user',(req,res)=>{
+    res.json("hello")
+})
 router.post('/userLogin',LoginUser)
 
 module.exports = router;
