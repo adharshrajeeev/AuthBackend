684a92ea692e17bddcc98e816c46761816a7f024
Adharsh R <adharshrajeeev2000@gmail.com>
2023-04-12 21:06:44 +0530

    req added

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ var router = express.Router();
 const { SignUpUser,LoginUser } = require('../controllers/userController');
 
 
-
-router.post('/signUp',SignUpUser);
-router.get('/user',(req,res)=>{
-    res.json("hello")
+router.get('/',(req,res)=>{
+    res.status(200).json("hey hey welcome")
 })
+router.get('/user',(req,res)=>{
+    res.status(200).json("hello")
+})
+router.post('/signUp',SignUpUser);
 router.post('/userLogin',LoginUser)
 
 module.exports = router;
